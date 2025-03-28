d4123828ef75f75021e3ae3c342e82623298d5c6
VoinBoria <voinboria@gmail.com>
2025-03-28 13:09:51 +0200

    Update DrawerContent.kt

--- DrawerContent.kt
+++ DrawerContent.kt
@@ -2,6 +2,7 @@
 package com.serhio.homeaccountingapp
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -9,7 +10,9 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -43,172 +46,186 @@ fun DrawerContent(
         else -> 240.dp  // Set a fixed width for larger screens
     }
 
-    Column(
+    val backgroundImage = painterResource(id = R.drawable.background_app) // Load the background image
+
+    Box(
         modifier = Modifier
             .fillMaxHeight()
             .width(drawerWidth)
-            .background(Color(0xFF1E1E1E).copy(alpha = 0.9f))
-            .padding(paddingSize)
-            .verticalScroll(rememberScrollState())  // Enable vertical scrolling
     ) {
-        Text(
-            text = stringResource(id = R.string.menu),
-            style = MaterialTheme.typography.titleLarge.copy(color = Color.White, fontSize = textSize)
+        Image(
+            painter = backgroundImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
-        Spacer(modifier = Modifier.height(20.dp))  // Adjust space for small screens
-        Column(modifier = Modifier.fillMaxWidth()) {
-            CategoryItem(
-                text = stringResource(id = R.string.main_menu),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_home),
-                        contentDescription = stringResource(id = R.string.main_menu_icon),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
-                    )
-                },
-                onClick = onNavigateToMainActivity,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
-                )
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(drawerWidth)
+                .background(Color(0xFF1E1E1E).copy(alpha = 0.8f)) // Added transparency to the background
+                .padding(paddingSize)
+                .verticalScroll(rememberScrollState())  // Enable vertical scrolling
+        ) {
+            Text(
+                text = stringResource(id = R.string.menu),
+                style = MaterialTheme.typography.titleLarge.copy(color = Color.White, fontSize = textSize)
             )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.incomes),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_income),
-                        contentDescription = stringResource(id = R.string.incomes_icon),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+            Spacer(modifier = Modifier.height(20.dp))  // Adjust space for small screens
+            Column(modifier = Modifier.fillMaxWidth()) {
+                CategoryItem(
+                    text = stringResource(id = R.string.main_menu),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_home),
+                            contentDescription = stringResource(id = R.string.main_menu_icon),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToMainActivity,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToIncomes,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.expenses),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_expense),
-                        contentDescription = stringResource(id = R.string.expenses_icon),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.incomes),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_income),
+                            contentDescription = stringResource(id = R.string.incomes_icon),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToIncomes,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToExpenses,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.all_income_transactions),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_all_income_transactions),
-                        contentDescription = stringResource(id = R.string.all_income_transactions),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.expenses),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_expense),
+                            contentDescription = stringResource(id = R.string.expenses_icon),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToExpenses,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToAllTransactionIncome,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.all_expense_transactions),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_all_expense_transactions),
-                        contentDescription = stringResource(id = R.string.all_expense_transactions),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.all_income_transactions),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_all_income_transactions),
+                            contentDescription = stringResource(id = R.string.all_income_transactions),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToAllTransactionIncome,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToAllTransactionExpense,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.issued_on_loan),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_loan_issued),
-                        contentDescription = stringResource(id = R.string.issued_on_loan_icon),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.all_expense_transactions),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_all_expense_transactions),
+                            contentDescription = stringResource(id = R.string.all_expense_transactions),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToAllTransactionExpense,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToIssuedOnLoan,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.borrowed),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_loan_borrowed),
-                        contentDescription = stringResource(id = R.string.borrowed_icon),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.issued_on_loan),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_loan_issued),
+                            contentDescription = stringResource(id = R.string.issued_on_loan_icon),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToIssuedOnLoan,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToBorrowed,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.budget_planning),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_budget_planning),
-                        contentDescription = stringResource(id = R.string.budget_planning_icon),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.borrowed),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_loan_borrowed),
+                            contentDescription = stringResource(id = R.string.borrowed_icon),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToBorrowed,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToBudgetPlanning,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            CategoryItem(
-                text = stringResource(id = R.string.task_manager),
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_task),
-                        contentDescription = stringResource(id = R.string.task_manager),
-                        tint = Color.White,
-                        modifier = Modifier.size(iconSize)
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.budget_planning),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_budget_planning),
+                            contentDescription = stringResource(id = R.string.budget_planning_icon),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToBudgetPlanning,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
                     )
-                },
-                onClick = onNavigateToTaskActivity,
-                gradientColors = listOf(
-                    Color(0xFF000000).copy(alpha = 0.7f),
-                    Color(0xFF2E2E2E).copy(alpha = 0.7f)
                 )
-            )
+                Spacer(modifier = Modifier.height(8.dp))
+                CategoryItem(
+                    text = stringResource(id = R.string.task_manager),
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_task),
+                            contentDescription = stringResource(id = R.string.task_manager),
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize)
+                        )
+                    },
+                    onClick = onNavigateToTaskActivity,
+                    gradientColors = listOf(
+                        Color(0xFF000000).copy(alpha = 0.7f),
+                        Color(0xFF2E2E2E).copy(alpha = 0.7f)
+                    )
+                )
+            }
         }
     }
 }
